1fdb8828db19ad97d0323b95d2b43402a18b8f9f
Hanna Winter <hanna.winter@ipa.fraunhofer.de>
2019-11-13 14:30:32 +0100

    changed code structure

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ If you want to save your completed annotation to an excel file for better readab
 ```bash
 $ pip3 install openpyxl
 ```
-Alternatively, change the function calling in lines 130 and 131 to save your results to a txt file. Either way, the annotation is saved to a pickle file.
+Alternatively, change the function calling in lines 189 and 190 to save your results to a txt file. Either way, the annotation is saved to a pickle file.
 
 ## Description
 The annotation tool works via the command line. It prints each sentence of your text file and lets you annotate word by word. To do so, it shows you word after word and takes your input as label. 
@@ -24,7 +24,7 @@ The annotation tool works via the command line. It prints each sentence of your 
 If you exit the tool, your progress so far is saved and you can continue with the last unannotated sentence next time you run the script. You can exit the tool any time but exiting during the annotaion of a senetence will reset the annotation of this one sentence.
 
 ### Pre-defined Labelset
-Before you start, you need to pre-define the labels you want to use for your annotations. You can do so in line 82:
+Before you start, you need to pre-define the labels you want to use for your annotations. You can do so in line 141:
 ```python
 labels = ("",)
 ```
@@ -46,7 +46,7 @@ There are a few commands you can issue while using the tool:
 * *sentence*: prints the sentence you are currently annotating
 
 #### Saving Your Progress
-Your annotation progress is automatically saved when exiting the tool. When you completely annotated the text in the given file, the results are saved to a conll file by default. You can change this format (see line 130) to a txt or excel file. 
+Your annotation progress is automatically saved when exiting the tool. When you completely annotated the text in the given file, the results are saved to a conll file by default. You can change this format (see line 189) to a txt or excel file. 
 
 #### Commandline Interface
 To start off at your last sentence, you can indicate its index as command line argument:
--- annotate.py
+++ annotate.py
@@ -18,130 +18,23 @@ from nltk.tokenize import word_tokenize
 from nltk.tokenize import sent_tokenize
 
 
+parser = argparse.ArgumentParser()
+#mandatory argument: text feil to be annotated
+parser.add_argument("file", help="Name a text file.", type=str)
+#optional argument: index of sentence that was annotated last
+parser.add_argument("-i", "--index", help="Indicate index where you left off.", type=str, default="0")
+args = parser.parse_args()
 
-def fetch_annotation(index):
-	"""load annotations and continue at last annotated sentence"""
-	if index != 0:
-		try:
-			annotation = pickle.load(open(file_name, "rb"))
-		except OSError:
-			annotation = []
-	else:
-		try:
-			#check if previous annotation exists to prevent overwriting
-			annotation = pickle.load(open(file_name, "rb"))
-			print("There exists an annotation for your file. Do you want to load it? [y/n]")
-			answer = input()
-			#answer must be either of: (y, n, yes, no)
-			valid_answer = False
-			while not valid_answer:
-				if answer == "y" or answer == "yes":
-					valid_answer = True
-					#annotation = pickle.load(open(file_name, "rb"))
-					#load last index to continue annotation
-					#check if there is an index from the last annotation
-					print("check")
-					with open("index.txt", "r") as f:
-						index = int(f.read())
-					print("You left off at sentence %s. Continue here? [y/n]" %str(index))
-					ans = input()
-					valid_ans = False
-					while not valid_ans:
-						if ans == "y" or ans == "yes":
-							valid_ans = True
-						elif ans == "n" or ans == "no":
-							valid_ans = True
-							print("At what index do you want to continue?")
-							index = input()
-							while not index.isdigit():
-								print("Index must be integer. Please type your last index.")
-								index = input()
-							index = int(index)
-						else:
-							print("Please answer with [y/n] or [yes/no].")
-							ans = input()
-				elif answer == "n" or answer == "no":
-					valid_answer = True
-					print("Starting annotation from first sentence.")
-					annotation = []
-				else:
-					print("Please answer with [y/n] or [yes/no].")
-					answer = input()
-		except OSError:
-			annotation = []
-	return annotation
+file = args.file
 
+#open file containing text to be annotated
+with open(file, "r") as f:
+	text = f.read()
 
+index = int(args.index)
 
-
-def annotate(index, text, annotation):
-	"""work flow for the annotation """
-	#get list of sentences
-	sentences = sent_tokenize(text)
-	#TODO: define your labels (to avoid empty labels delete "" from tuple)	
-	labels = ("",)
-	#iterate over sentences and their words and take input as label
-	for s in sentences[index:]:
-		i = sentences.index(s)
-		print("Press enter to continue or write 'exit' to stop annotation.")
-		user = input()
-		if user == "exit":
-			break
-		print(s)
-		words = word_tokenize(s)
-		anno_sent = []
-		for w in words:
-			print(w)
-			label = input()
-			i_w = words.index(w)
-			while label not in labels:
-				if label == "sentence":
-					print(s)
-					print(w)
-					label = input()
-				elif label == "annotation":
-					print(annotation)
-					print(w)
-					label = input()
-				elif label == "exit":
-					break
-				else:
-				#only accepts pre-defined labels , or "exit"
-					print("Label not valid. Options: %s" %str(labels))
-					print(w)
-					label = input()
-			if label == "exit":
-				print("Exiting annotation.")
-				index = i
-				break
-			elif label in labels:
-				anno_sent.append((w, label))
-		if len(anno_sent) == len(words):
-			annotation.append(anno_sent)
-		i_w = 0
-		if label == "exit":
-			save_progress(annotation, index)
-			break
-		elif label == "sentence":
-			print(s)
-		if len(sentences) == sentences.index(s)+1:
-			print("Annotation complete!")
-			#save_anno_to_txt(annotation)
-			save_anno_to_excel(annotation)
-			save_as_conll(annotation)
-
-
-
-def save_progress(annotation, index):
-	"""saves progress with annotation and index"""
-	#save new annotation to pickle file
-	with open (file_name, "wb") as fp:
-		pickle.dump(annotation, fp)
-	#save index to file
-	with open("index.txt", "w") as f:
-		f.write(str(index))
-	print("Your annotation was saved to: %s ." %file_name)
-	print("The index of your last annotated sentence was saved: %s ." %index)
+#og filename is taken for new file containing annotations
+file_name = file[:-4] + "-ANNO.pickle"
 
 
 
@@ -189,32 +82,128 @@ def save_as_conll(annotation):
 
 
 
-if __name__ == '__main__':
+#load annotations and continue at last annotated sentence
+if index != 0:
+	try:
+		annotation = pickle.load(open(file_name, "rb"))
+	except OSError:
+		annotation = []
+else:
+	try:
+		#check if previous annotation exists to prevent overwriting
+		annotation = pickle.load(open(file_name, "rb"))
+		print("There exists an annotation for your file. Do you want to load it? [y/n]")
+		answer = input()
+		#answer must be either of: (y, n, yes, no)
+		valid_answer = False
+		while not valid_answer:
+			if answer == "y" or answer == "yes":
+				valid_answer = True
+				#annotation = pickle.load(open(file_name, "rb"))
+				#load last index to continue annotation
+				#check if there is an index from the last annotation
+				print("check")
+				with open("index.txt", "r") as f:
+					index = int(f.read())
+				print("You left off at sentence %s. Continue here? [y/n]" %str(index))
+				ans = input()
+				valid_ans = False
+				while not valid_ans:
+					if ans == "y" or ans == "yes":
+						valid_ans = True
+					elif ans == "n" or ans == "no":
+						valid_ans = True
+						print("At what index do you want to continue?")
+						index = input()
+						while not index.isdigit():
+							print("Index must be integer. Please type your last index.")
+							index = input()
+						index = int(index)
+					else:
+						print("Please answer with [y/n] or [yes/no].")
+						ans = input()
+			elif answer == "n" or answer == "no":
+				valid_answer = True
+				print("Starting annotation from first sentence.")
+				annotation = []
+			else:
+				print("Please answer with [y/n] or [yes/no].")
+				answer = input()
+	except OSError:
+		annotation = []
 
-	parser = argparse.ArgumentParser()
-	#mandatory argument: text feil to be annotated
-	parser.add_argument("file", help="Name a text file.", type=str)
-	#optional argument: index of sentence that was annotated last
-	parser.add_argument("-i", "--index", help="Indicate index where you left off.", type=str, default="0")
-	args = parser.parse_args()
-
-	file = args.file
-
-	#open file containing text to be annotated
-	with open(file, "r") as f:
-		text = f.read()
-
-	index = int(args.index)
-
-	#og filename is taken for new file containing annotations
-	file_name = file[:-4] + "-ANNO.pickle"
-
-	annotation = fetch_annotation(index)
-
-	annotate(index, text, annotation)
-
-	#print(annotation)
 
 
 
+#get list of sentences
+sentences = sent_tokenize(text)
+#TODO: define your labels (to avoid empty labels delete "" from tuple)	
+labels = ("",)
+
+#iterate over sentences and their words and take input as label
+for s in sentences[index:]:
+	i = sentences.index(s)
+	print("Press enter to continue or write 'exit' to stop annotation.")
+	user = input()
+	if user == "exit":
+		break
+	print(s)
+	words = word_tokenize(s)
+	anno_sent = []
+	for w in words:
+		print(w)
+		label = input()
+		i_w = words.index(w)
+		while label not in labels:
+			if label == "sentence":
+				print(s)
+				print(w)
+				label = input()
+			elif label == "annotation":
+				print(annotation)
+				print(w)
+				label = input()
+			elif label == "exit":
+				break
+			else:
+			#only accepts pre-defined labels , or "exit"
+				print("Label not valid. Options: %s" %str(labels))
+				print(w)
+				label = input()
+		if label == "exit":
+			print("Exiting annotation.")
+			index = i
+			break
+		elif label in labels:
+			anno_sent.append((w, label))
+	if len(anno_sent) == len(words):
+		annotation.append(anno_sent)
+	i_w = 0
+	if label == "exit":
+		break
+	elif label == "sentence":
+		print(s)
+	if len(sentences) == sentences.index(s)+1:
+		print("Annotation complete!")
+		#save_anno_to_txt(annotation)
+		save_anno_to_excel(annotation)
+		save_as_conll(annotation)
+
+
+
+
+#save new annotation to pickle file
+with open (file_name, "wb") as fp:
+	pickle.dump(annotation, fp)
+
+#save index to file
+with open("index.txt", "w") as f:
+	f.write(str(index))
+
+print("Your annotation was saved to: %s ." %file_name)
+print("The index of your last annotated sentence was saved: %s ." %index)
+
+
+
+#print(annotation)
 
